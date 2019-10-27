c6eb9ad92a9d7b90f7b14f8ecbc2438ddd3a057b
Christopher Yarp <cyarp@eecs.berkeley.edu>
2019-10-27 16:02:51 -0700

    Adding pipeBench

---
+++ pipeBench/src/helpers.h
@@ -0,0 +1,13 @@
+//
+// Created by Christopher Yarp on 10/27/19.
+//
+
+#ifndef PIPEBENCH_HELPERS_H
+#define PIPEBENCH_HELPERS_H
+
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isReadyForReading(FILE* file);
+
+#endif //PIPEBENCH_HELPERS_H

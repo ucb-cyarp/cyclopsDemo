404f8c7f265ef6491b2dd3cda63a66f40812e58c
Christopher Yarp <cyarp@eecs.berkeley.edu>
2021-11-07 15:28:21 -0800

    Settings for BWRC Retreat Demo Fall 2021

--- scripts/runDemoTmuxSharedMem.sh
+++ scripts/runDemoTmuxSharedMem.sh
@@ -5,7 +5,7 @@
 RxSrc=rev1BB_receiver
 TxSrc=rev1BB_transmitter
 cyclopsASCIIDir=../../submodules/cyclopsASCIILink-sharedMem
-uhdToPipesDir=../../submodules/uhdToPipes
+bladeRFToFIFODir=../../submodules/bladeRFToFIFO
 dummyAdcDacDir=../../dummyAdcDacSharedMemFIFO
 BlockSize=120
 #BlockSize=64
@@ -22,7 +22,9 @@ appCPU=7 # Epyc 3000 & Ryzen 3000 (New Bios)
 # appCPU=4 # Ryzen 3000 (Old Bios)
 TxTokens=1000
 txdutycycle=1.00
-rxsubsampleperiod=2000
+# rxsubsampleperiod=2000
+rxsubsampleperiod=100
+# rxsubsampleperiod=10
 
 #ProcessLimitCyclops=10
 ProcessLimitCyclops=100
@@ -42,11 +44,22 @@ rxCPU=6
 # txCPU=2
 # rxCPU=3
 
-usrpArgs="addr=192.168.40.2"
 Freq=5800000000
-Rate=4000000
-TxGainDB=30
-RxGainDB=30
+#Low Rate
+# Rate=4000000
+# BW=6000000
+#Max Reccomended BladeRF Rate 1x1 Tx/Rx simultanious
+Rate=40000000
+BW=40000000
+#Max Rate
+# Rate=61440000
+# BW=56000000
+TxGainDB=50
+RxGainDB=45
+#For BladeRF Only
+FullScale=2.0
+#For USRP Only
+usrpArgs="addr=192.168.40.2"
 txChan=0
 rxChan=1
 
@@ -54,13 +67,13 @@ curDir=`pwd`
 RxDir=${curDir}/cOut_${RxSrc}
 TxDir=${curDir}/cOut_${TxSrc}
 cyclopsASCIIBuildDir=${cyclopsASCIIDir}/build
-uhdToPipesBuildDir=${uhdToPipesDir}/build
+bladeRFToFIFOBuildDir=${bladeRFToFIFODir}/build
 dummyAdcDacBuildDir=${dummyAdcDacDir}/build
 
 if [ ${USE_DUMMY} -ne 0 ]; then
     echo "Demo with Dummy ADC/DAC"
 else
-    echo "Demo with USRP/UHD"
+    echo "Demo with bladeRF"
 fi
 
 if [[ -d "demoRun" ]]; then
@@ -80,7 +93,7 @@ echo "${RxDir}/benchmark_rx_demo_io_posix_shared_mem"
 cd ..
 mkdir tx
 cd tx
-tmux split-window -h -d -t vitis_cyclopse_demo:0 "printf '\\033]2;%s\\033\\\\' 'Tx_DSP'; module load papi; ${TxDir}/benchmark_tx_demo_io_posix_shared_mem"
+tmux split-window -h -d -t vitis_cyclopse_demo:0 "printf '\\033]2;%s\\033\\\\' 'Tx_DSP'; module load papi; ${TxDir}/benchmark_tx_demo_io_posix_shared_mem; sleep 5"
 # tmux rename-window -t vitis_cyclopse_demo:1 'tx'
 echo "${TxDir}/benchmark_tx_demo_io_posix_shared_mem"
 cd ..
@@ -93,20 +106,20 @@ sleep 5
 
 #Start cyclopsASCII (before the ADC/DAC)
 #Feedback backpressure will prevent a runaway
-tmux split-window -v -d -t vitis_cyclopse_demo:0 "printf '\\033]2;%s\\033\\\\' 'Cyclops_ASCII_Application'; ${cyclopsASCIIBuildDir}/cyclopsASCIILink -rx ${vitisFromRxPipe} -tx ${vitisToTxPipe} -txfb ${TxFeedbkAppPipeName} -txtokens ${TxTokens} -cpu ${appCPU} -processlimit ${ProcessLimitCyclops} -txdutycycle ${txdutycycle} -rxsubsampleperiod ${rxsubsampleperiod} -fifosize ${IO_FIFO_SIZE}"
+tmux split-window -v -d -t vitis_cyclopse_demo:0 "printf '\\033]2;%s\\033\\\\' 'Cyclops_ASCII_Application'; ${cyclopsASCIIBuildDir}/cyclopsASCIILink -rx ${vitisFromRxPipe} -tx ${vitisToTxPipe} -txfb ${TxFeedbkAppPipeName} -txtokens ${TxTokens} -cpu ${appCPU} -processlimit ${ProcessLimitCyclops} -txdutycycle ${txdutycycle} -rxsubsampleperiod ${rxsubsampleperiod} -fifosize ${IO_FIFO_SIZE}; sleep 5"
 # tmux rename-window -t vitis_cyclopse_demo:2 'cyclopsASCII'
 echo "${cyclopsASCIIBuildDir}/cyclopsASCIILink -rx ${vitisFromRxPipe} -tx ${vitisToTxPipe} -txfb ${TxFeedbkAppPipeName} -txtokens ${TxTokens} -cpu ${appCPU} -processlimit ${ProcessLimitCyclops} -txdutycycle ${txdutycycle} -rxsubsampleperiod ${rxsubsampleperiod} -fifosize ${IO_FIFO_SIZE}"
 
 if [ ${USE_DUMMY} -ne 0 ]; then
     #Start dummyAdcDac
-    tmux split-window -v -d -t vitis_cyclopse_demo:0.2 "printf '\\033]2;%s\\033\\\\' 'Dummy_DAC/DAC'; ${dummyAdcDacBuildDir}/dummyAdcDacSharedMemFIFO -cpu ${txCPU} -rx ${vitisFromADCPipe} -tx ${vitisToDACPipe} -txfb ${TxFeedbkAppPipeName} -blocklen ${BlockSize} -fifosize ${IO_FIFO_SIZE}"
+    tmux split-window -v -d -t vitis_cyclopse_demo:0.2 "printf '\\033]2;%s\\033\\\\' 'Dummy_DAC/DAC'; ${dummyAdcDacBuildDir}/dummyAdcDacSharedMemFIFO -cpu ${txCPU} -rx ${vitisFromADCPipe} -tx ${vitisToDACPipe} -txfb ${TxFeedbkAppPipeName} -blocklen ${BlockSize} -fifosize ${IO_FIFO_SIZE}; sleep 5"
     # tmux rename-window -t vitis_cyclopse_demo:3 'dummyAdcDac'
     echo "${dummyAdcDacBuildDir}/dummyAdcDacSharedMemFIFO -cpu ${txCPU} -rx ${vitisFromADCPipe} -tx ${vitisToDACPipe} -txfb ${TxFeedbkAppPipeName} -blocklen ${BlockSize} -fifosize ${IO_FIFO_SIZE}"
 else
-    # #Start uhdToPipes
-    tmux split-window -v -d -t vitis_cyclopse_demo:0.2 "printf '\\033]2;%s\\033\\\\' 'UHD/USRP'; module load uhd; ${uhdToPipesBuildDir}/uhdToPipes -a ${usrpArgs} -f ${Freq} -r ${Rate} --txgain ${TxGainDB} --rxgain ${RxGainDB}  --uhdcpu ${uhdCPU} --txcpu ${txCPU} --rxcpu ${rxCPU} --rxpipe ${vitisFromADCPipe} --txpipe ${vitisToDACPipe} --txfeedbackpipe ${TxFeedbkAppPipeName} --samppertransactrx ${BlockSize} --samppertransacttx ${BlockSize} --forcefulltxbuffer --txchan ${txChan} --rxchan ${rxChan} --txratelimit --fifosize ${IO_FIFO_SIZE}"
-    # tmux rename-window -t vitis_cyclopse_demo:3 'uhdToPipes'
-    echo "${uhdToPipesBuildDir}/uhdToPipes -a ${usrpArgs} -f ${Freq} -r ${Rate} --txgain ${TxGainDB} --rxgain ${RxGainDB} --uhdcpu ${uhdCPU} --txcpu ${txCPU} --rxcpu ${rxCPU} --rxpipe ${vitisFromADCPipe} --txpipe ${vitisToDACPipe} --txfeedbackpipe ${TxFeedbkAppPipeName} --samppertransactrx ${BlockSize} --samppertransacttx ${BlockSize} --forcefulltxbuffer --txchan ${txChan} --rxchan ${rxChan} --txratelimit --fifosize ${IO_FIFO_SIZE}"
+    # #Start bladeRFToFIFO
+    tmux split-window -v -d -t vitis_cyclopse_demo:0.2 "printf '\\033]2;%s\\033\\\\' 'bladeRF'; module load bladeRF; ${bladeRFToFIFOBuildDir}/bladeRFToFIFO -txFreq ${Freq} -rxFreq ${Freq} -txSampRate ${Rate} -rxSampRate ${Rate} -txBW ${BW} -rxBW ${BW} -txGain ${TxGainDB} -rxGain ${RxGainDB} -txCpu ${txCPU} -rxCpu ${rxCPU} -rx ${vitisFromADCPipe} -tx ${vitisToDACPipe} -txfb ${TxFeedbkAppPipeName} -blocklen ${BlockSize} -fifosize ${IO_FIFO_SIZE} -fullScale ${FullScale} -saturate -v; sleep 100"
+    # tmux rename-window -t vitis_cyclopse_demo:3 'bladeRFToFIFO'
+    echo "${bladeRFToFIFOBuildDir}/bladeRFToFIFO -txFreq ${Freq} -rxFreq ${Freq} -txSampRate ${Rate} -rxSampRate ${Rate} -txBW ${BW} -rxBW ${BW} -txGain ${TxGainDB} -rxGain ${RxGainDB} -txCpu ${txCPU} -rxCpu ${rxCPU} -rx ${vitisFromADCPipe} -tx ${vitisToDACPipe} -txfb ${TxFeedbkAppPipeName} -blocklen ${BlockSize} -fifosize ${IO_FIFO_SIZE} -fullScale ${FullScale} -saturate -v"
 fi
 
 cd ${curDir}

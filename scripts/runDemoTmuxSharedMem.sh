a16948e59e5b843df14c7d53aa2eee7ee05cf0e7
Christopher Yarp <cyarp@eecs.berkeley.edu>
2022-02-28 14:32:21 -0800

    Updating demo to rev1_40 of the cyclopsBB (https://github.com/ucb-cyarp/cyclopsbb/commit/361aef752817baf6a8405fc7c8a97bf4b3ea980c).  Modifications for bladeRF as well.

--- scripts/runDemoTmuxSharedMem.sh
+++ scripts/runDemoTmuxSharedMem.sh
@@ -45,23 +45,94 @@ rxCPU=6
 # rxCPU=3
 
 Freq=2400000000
+#CSV Dump Rate
+Rate=1000000
+BW=1000000
 #Low Rate
 # Rate=4000000
 # BW=6000000
 #Max Reccomended BladeRF Rate 1x1 Tx/Rx simultanious
-Rate=40000000
-BW=40000000
+# Rate=40000000
+# BW=40000000
 #Max Rate
 # Rate=61440000
 # BW=56000000
-TxGainDB=35
-RxGainDB=45
+TxGainDB=45
+RxGainDB=35
 #For BladeRF Only
-FullScale=2.0
+FullScale=1.75
 #For USRP Only
 usrpArgs="addr=192.168.40.2"
 txChan=0
 rxChan=1
+
+## BladeRF IQ Params
+# BLADERF_A_SERIAL="3baf359b82904cb4967f4c275f174b88"
+# BLADERF_A_RX_DC_I="-0.64618"
+# BLADERF_A_RX_DC_Q="-0.15726"
+# BLADERF_A_RX_IQ_GAIN="0.86432"
+# BLADERF_A_RX_IQ_PHASE_DEG="-1.7253"
+# BLADERF_A_TX_DC_I="0.18758"
+# BLADERF_A_TX_DC_Q="-0.10816"
+# BLADERF_A_TX_IQ_GAIN="0.9839"
+# BLADERF_A_TX_IQ_PHASE_DEG="-0.010293"
+
+BLADERF_B_SERIAL="72e930b12d1441ba861ed8f0396f0238"
+BLADERF_B_RX_DC_I="-0.055969"
+BLADERF_B_RX_DC_Q="0.7084"
+BLADERF_B_RX_IQ_GAIN="1.0871"
+BLADERF_B_RX_IQ_PHASE_DEG="-1.3382"
+BLADERF_B_TX_DC_I="0.6388"
+BLADERF_B_TX_DC_Q="0.95944"
+BLADERF_B_TX_IQ_GAIN="0.98044"
+BLADERF_B_TX_IQ_PHASE_DEG="-0.0055925"
+
+BLADERF_A_SERIAL="3baf359b82904cb4967f4c275f174b88"
+BLADERF_A_RX_DC_I="0"
+BLADERF_A_RX_DC_Q="0"
+BLADERF_A_RX_IQ_GAIN="1"
+BLADERF_A_RX_IQ_PHASE_DEG="0"
+BLADERF_A_TX_DC_I="0"
+BLADERF_A_TX_DC_Q="0"
+BLADERF_A_TX_IQ_GAIN="1"
+BLADERF_A_TX_IQ_PHASE_DEG="0"
+
+BLADERF_TX="A" #A or B
+BLADERF_RX="A" #A or B
+
+if [[ ${BLADERF_TX} == "A" ]]; then
+    BLADERF_TX_SERIAL=${BLADERF_A_SERIAL};
+    BLADERF_TX_DC_I=${BLADERF_A_TX_DC_I};
+    BLADERF_TX_DC_Q=${BLADERF_A_TX_DC_Q};
+    BLADERF_TX_IQ_GAIN=${BLADERF_A_TX_IQ_GAIN};
+    BLADERF_TX_IQ_PHASE_DEG=${BLADERF_A_TX_IQ_PHASE_DEG};
+elif [[ ${BLADERF_TX} == "B" ]]; then
+    BLADERF_TX_SERIAL=${BLADERF_B_SERIAL};
+    BLADERF_TX_DC_I=${BLADERF_B_TX_DC_I};
+    BLADERF_TX_DC_Q=${BLADERF_B_TX_DC_Q};
+    BLADERF_TX_IQ_GAIN=${BLADERF_B_TX_IQ_GAIN};
+    BLADERF_TX_IQ_PHASE_DEG=${BLADERF_B_TX_IQ_PHASE_DEG};
+else
+    echo "Unknown BladeRF_TX!"
+    exit 1
+fi
+
+if [[ ${BLADERF_RX} == "A" ]]; then
+    BLADERF_RX_SERIAL=${BLADERF_A_SERIAL};
+    BLADERF_RX_DC_I=${BLADERF_A_RX_DC_I};
+    BLADERF_RX_DC_Q=${BLADERF_A_RX_DC_Q};
+    BLADERF_RX_IQ_GAIN=${BLADERF_A_RX_IQ_GAIN};
+    BLADERF_RX_IQ_PHASE_DEG=${BLADERF_A_RX_IQ_PHASE_DEG};
+elif [[ ${BLADERF_RX} == "B" ]]; then
+    BLADERF_RX_SERIAL=${BLADERF_B_SERIAL};
+    BLADERF_RX_DC_I=${BLADERF_B_RX_DC_I};
+    BLADERF_RX_DC_Q=${BLADERF_B_RX_DC_Q};
+    BLADERF_RX_IQ_GAIN=${BLADERF_B_RX_IQ_GAIN};
+    BLADERF_RX_IQ_PHASE_DEG=${BLADERF_B_RX_IQ_PHASE_DEG};
+else
+    echo "Unknown BladeRF_TX!"
+    exit 1
+fi
 
 curDir=`pwd`
 RxDir=${curDir}/cOut_${RxSrc}
@@ -117,9 +188,9 @@ if [ ${USE_DUMMY} -ne 0 ]; then
     echo "${dummyAdcDacBuildDir}/dummyAdcDacSharedMemFIFO -cpu ${txCPU} -rx ${vitisFromADCPipe} -tx ${vitisToDACPipe} -txfb ${TxFeedbkAppPipeName} -blocklen ${BlockSize} -fifosize ${IO_FIFO_SIZE}"
 else
     # #Start bladeRFToFIFO
-    tmux split-window -v -d -t vitis_cyclopse_demo:0.2 "printf '\\033]2;%s\\033\\\\' 'bladeRF'; module load bladeRF; ${bladeRFToFIFOBuildDir}/bladeRFToFIFO -txFreq ${Freq} -rxFreq ${Freq} -txSampRate ${Rate} -rxSampRate ${Rate} -txBW ${BW} -rxBW ${BW} -txGain ${TxGainDB} -rxGain ${RxGainDB} -txCpu ${txCPU} -rxCpu ${rxCPU} -rx ${vitisFromADCPipe} -tx ${vitisToDACPipe} -txfb ${TxFeedbkAppPipeName} -blocklen ${BlockSize} -fifosize ${IO_FIFO_SIZE} -fullScale ${FullScale} -saturate -v; sleep 100"
+    tmux split-window -v -d -t vitis_cyclopse_demo:0.2 "printf '\\033]2;%s\\033\\\\' 'bladeRF'; module load bladeRF; ${bladeRFToFIFOBuildDir}/bladeRFToFIFO -txFreq ${Freq} -rxFreq ${Freq} -txSampRate ${Rate} -rxSampRate ${Rate} -txBW ${BW} -rxBW ${BW} -txGain ${TxGainDB} -rxGain ${RxGainDB} -txCpu ${txCPU} -rxCpu ${rxCPU} -rx ${vitisFromADCPipe} -tx ${vitisToDACPipe} -txfb ${TxFeedbkAppPipeName} -blocklen ${BlockSize} -fifosize ${IO_FIFO_SIZE} -fullScale ${FullScale} -saturate -txSerialNum ${BLADERF_TX_SERIAL} -rxSerialNum ${BLADERF_RX_SERIAL} -txDCOffsetI ${BLADERF_TX_DC_I} -txDCOffsetQ ${BLADERF_TX_DC_Q} -rxDCOffsetI ${BLADERF_RX_DC_I} -rxDCOffsetQ ${BLADERF_RX_DC_Q} -txIQGain ${BLADERF_TX_IQ_GAIN} -txIQPhase ${BLADERF_TX_IQ_PHASE_DEG} -rxIQGain ${BLADERF_RX_IQ_GAIN} -rxIQPhase ${BLADERF_RX_IQ_PHASE_DEG} -v; sleep 100"
     # tmux rename-window -t vitis_cyclopse_demo:3 'bladeRFToFIFO'
-    echo "${bladeRFToFIFOBuildDir}/bladeRFToFIFO -txFreq ${Freq} -rxFreq ${Freq} -txSampRate ${Rate} -rxSampRate ${Rate} -txBW ${BW} -rxBW ${BW} -txGain ${TxGainDB} -rxGain ${RxGainDB} -txCpu ${txCPU} -rxCpu ${rxCPU} -rx ${vitisFromADCPipe} -tx ${vitisToDACPipe} -txfb ${TxFeedbkAppPipeName} -blocklen ${BlockSize} -fifosize ${IO_FIFO_SIZE} -fullScale ${FullScale} -saturate -v"
+    echo "${bladeRFToFIFOBuildDir}/bladeRFToFIFO -txFreq ${Freq} -rxFreq ${Freq} -txSampRate ${Rate} -rxSampRate ${Rate} -txBW ${BW} -rxBW ${BW} -txGain ${TxGainDB} -rxGain ${RxGainDB} -txCpu ${txCPU} -rxCpu ${rxCPU} -rx ${vitisFromADCPipe} -tx ${vitisToDACPipe} -txfb ${TxFeedbkAppPipeName} -blocklen ${BlockSize} -fifosize ${IO_FIFO_SIZE} -fullScale ${FullScale} -saturate -txSerialNum ${BLADERF_TX_SERIAL} -rxSerialNum ${BLADERF_RX_SERIAL} -txDCOffsetI ${BLADERF_TX_DC_I} -txDCOffsetQ ${BLADERF_TX_DC_Q} -rxDCOffsetI ${BLADERF_RX_DC_I} -rxDCOffsetQ ${BLADERF_RX_DC_Q} -txIQGain ${BLADERF_TX_IQ_GAIN} -txIQPhase ${BLADERF_TX_IQ_PHASE_DEG} -rxIQGain ${BLADERF_RX_IQ_GAIN} -rxIQPhase ${BLADERF_RX_IQ_PHASE_DEG} -v"
 fi
 
 cd ${curDir}

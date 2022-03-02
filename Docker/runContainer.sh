08437a8e172d31c5b60a7b558278582cfe902144
Christopher Yarp <cyarp@eecs.berkeley.edu>
2022-03-02 12:37:48 -0800

    Making docker scripts more friendly when working with a docker image repository

--- Docker/runContainer.sh
+++ Docker/runContainer.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -ti --rm -p 8000:8000 cyclops_demo_local
+#Will run the latest version of the container image
+#Binds port 8000 in the container to port 8000 on localhost
+#for viewing telemetry dashboard in web browser
+#Go to https://127.0.0.1:8000 once container is running
+docker run -ti --rm -p 8000:8000 cyclops_demo_local:latest
